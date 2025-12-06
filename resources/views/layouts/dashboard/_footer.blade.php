908d5be66bc79fe7b06b846fdc40c0d749b33110
nada <nada.ibrahim@social-eyez.com>
2025-12-06 20:51:46 +0200

    logo

--- resources/views/layouts/dashboard/_footer.blade.php
+++ resources/views/layouts/dashboard/_footer.blade.php
@@ -2,9 +2,9 @@
     <div class="row g-0 justify-content-between fs--1 mt-4 mb-3">
         <div class="col-12 col-sm-auto text-center" style="direction: ltr !important">
             <p class="mb-0 text-600">
-                &copy; <img src="storage/images/users/nada.png" alt="Logo" style="height: 20px;">
+                &copy; <img src="{{ asset('storage/images/users/nada.png') }}" alt="Logo" style="height: 20px;">
                 <a href="https://nada-ibrahim.com" target="_blank"> <span style="color: red;">nada-ibrahim.com</span></a>
-                . All Rights Reserved | 2023
+                . All Rights Reserved | 2025
             </p>
         </div>
         <div class="col-12 col-sm-auto text-center">
